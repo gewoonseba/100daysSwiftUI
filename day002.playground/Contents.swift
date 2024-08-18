0f1f6aa1f163d7c2ef68c13253dfaad2eb84689d
Sebastian Stoelen <gewoonseba@gmail.com>
2024-08-18 11:46:05 +0200

    day 2

---
+++ day002.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Cocoa
+
+let number = 120
+print (number.isMultiple(of: 3))
+
+let goodDogs = true
+var gameOver = false
+gameOver.toggle()
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+
+
+var first = "Hello "
+var last = "World"
+let greeting = first + last
+
+let name = "Taylor"
+let age = 26
+
+let message = "Hello, my name is \(name), and I'm \(age) years old."
+print(message)
+
+let celciusTemp = 18.5
+let fahrenHeitTemp = celciusTemp * 9 / 5 + 32
+
+print("The temperature today is \(celciusTemp)°C, or \(fahrenHeitTemp)°F. ☀️")
