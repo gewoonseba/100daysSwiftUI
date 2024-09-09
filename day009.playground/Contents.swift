b63bbde2e6b4ad4f0d71d3cedfbf39d7e6f8d793
Sebastian Stoelen <gewoonseba@gmail.com>
2024-09-09 12:06:08 +0200

    day9 done

--- day009.playground/Contents.swift
+++ day009.playground/Contents.swift
@@ -130,3 +130,14 @@ doImportantwork {
 } third: {
     print("This is the actual third work ,")
 }
+
+// Checkpoint 5
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+luckyNumbers
+    .filter { !$0.isMultiple(of: 2) }
+    .sorted()
+    .map { print("\($0) is a lucky number") }
+
+print(luckyNumbers)
