88149dfa720db6c25f234d91f28daef1ba0c33b4
Sebastian Stoelen <gewoonseba@gmail.com>
2024-09-17 17:58:14 +0200

    Initialized wesplit project

---
+++ Projects/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sebastian Stoelen on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
