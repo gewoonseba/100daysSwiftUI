6287850cd231f415cc209c483139df54f7ba8f7f
Sebastian Stoelen <gewoonseba@gmail.com>
2024-09-19 19:36:54 +0200

    end of day 18

--- Projects/WeSplit/WeSplit/ContentView.swift
+++ Projects/WeSplit/WeSplit/ContentView.swift
@@ -9,45 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
   @FocusState private var amountIsfocused: Bool
-  
+
   @State private var checkAmount = 0.0
   @State private var numberOfPeople = 0
   @State private var tipPercentage = 20
 
-  private let availablePercentages = [0, 10, 15, 20, 25]
+  private let availablePercentages = 0...100
   
+  var totalIncludingTip: Double {
+    checkAmount + (checkAmount * Double(tipPercentage) / 100)
+  }
+
   var totalPerPerson: Double {
     let actualPeople = Double(numberOfPeople + 2)
-    let totalIncludingTip = Double(checkAmount + (checkAmount * Double(tipPercentage) / 100))
     return totalIncludingTip / actualPeople
   }
 
   var body: some View {
     NavigationStack {
       Form {
-        Section {
-          TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            .keyboardType(.decimalPad)
-            .focused($amountIsfocused)
-
+        Section("Check details") {
+          HStack {
+            Text("Check amount")
+            Spacer()
+            TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+              .keyboardType(.decimalPad)
+              .multilineTextAlignment(.trailing)
+              .focused($amountIsfocused)
+          }
           Picker("Number of people", selection: $numberOfPeople) {
             ForEach(2 ..< 100) {
               Text("\($0) people")
             }
           }.pickerStyle(.automatic)
-        }
-        
-        Section("How much do you want to tip?") {
-        
-          Picker("Tip percentage", selection: $tipPercentage) {
-            ForEach(availablePercentages, id: \.self) {
+          
+          Picker("Desired tip", selection: $tipPercentage) {
+            ForEach(availablePercentages, id: \.self){
               Text($0, format: .percent)
             }
-          }.pickerStyle(.segmented)
+          }
+          .pickerStyle(.navigationLink)
         }
 
-        Section {
-          Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        Section("Payment details") {
+          HStack {
+            Text("Total including tip")
+            Spacer()
+            Text(totalIncludingTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+          }
+          HStack {
+            Text("Total per person")
+            Spacer()
+            Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+          }
+          
         }
       }
       .navigationTitle("WeSplit")
