ae3b639771f8a3bdbbcf8d7c2e6ad7a2e9775e67
Sebastian Stoelen <gewoonseba@gmail.com>
2024-09-27 07:18:59 +0200

    start of day 26

---
+++ Projects/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sebastian Stoelen on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
