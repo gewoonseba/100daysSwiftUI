66538ba2e68dae9009ac1d92c925c68456d5da50
Sebastian Stoelen <gewoonseba@gmail.com>
2024-10-05 08:56:00 +0200

    Finished day 32

---
+++ Projects/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Sebastian Stoelen on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/Animations/Animations/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Sebastian Stoelen on 05/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  @State private var animationAmount = 0.0
+
+    var body: some View {
+        Button("Tap Me") {
+          withAnimation(.spring(duration: 1, bounce: 0.5)) {
+              animationAmount += 360
+          }
+
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
+
+    }
+}
+
+#Preview {
+  ContentView()
+}
