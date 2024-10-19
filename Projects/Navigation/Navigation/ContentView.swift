ea8ce664e165ec8f3201d20f275c9e1faf9427b2
Sebastian Stoelen <gewoonseba@gmail.com>
2024-10-19 16:29:05 +0200

    day45

--- Projects/Navigation/Navigation/ContentView.swift
+++ Projects/Navigation/Navigation/ContentView.swift
@@ -7,31 +7,14 @@
 
 import SwiftUI
 
-struct DetailView: View {
-    var number: Int
-    
-    @Binding var path: [Int]
-
-    var body: some View {
-        NavigationLink("Go to Random Number", value: Int.random(in: 1 ... 1000))
-            .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path.removeAll()
-                }
-            }
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore()
+    @State private var title = "SwiftUI"
 
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i,path: $pathStore.path)
-                }
+        NavigationStack {
+            Text("Hello, world!")
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
