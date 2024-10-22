7c49640bb1d3e37a6b3f5e00eb346ef1c13415bd
Sebastian Stoelen <gewoonseba@gmail.com>
2024-10-22 18:21:26 +0200

    day 47: save habits to userdefaults

--- Projects/Habito/Habito/Habit.swift
+++ Projects/Habito/Habito/Habit.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-struct Habit: Identifiable {
-    let id = UUID()
+struct Habit: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let description: String
     var count: Int = 0
--- Projects/Habito/Habito/UserHabits.swift
+++ Projects/Habito/Habito/UserHabits.swift
@@ -9,5 +9,21 @@ import Foundation
 
 @Observable
 class UserHabits {
-    var habits: [Habit] = []
+    var habits: [Habit] = [] {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(habits) {
+                UserDefaults.standard.set(encoded, forKey: "habits")
+            }
+        }
+    }
+
+    init() {
+        if let encoded = UserDefaults.standard.data(forKey: "habits") {
+            if let decoded = try? JSONDecoder().decode([Habit].self, from: encoded) {
+                habits = decoded
+                return
+            }
+        }
+        habits = []
+    }
 }
