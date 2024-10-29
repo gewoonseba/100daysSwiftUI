c742988b0b7413448a706579d19f5c596c9ccb4b
Sebastian Stoelen <gewoonseba@gmail.com>
2024-10-29 07:30:46 +0100

    day 53 part 1

---
+++ Projects/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Sebastian Stoelen on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
