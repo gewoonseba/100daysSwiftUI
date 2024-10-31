7effb10a7e344085976e426be48d5da7e85b7071
Sebastian Stoelen <gewoonseba@gmail.com>
2024-10-31 08:59:20 +0100

    day 55 part 1

---
+++ Projects/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,59 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Sebastian Stoelen on 31/10/2024.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(book.genre.uppercased())
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.ultraThinMaterial)
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+
+            Text(book.review)
+                .padding()
+
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        // modelcontainer needs to be created to create and store a book example in memory
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it.", rating: 4)
+
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
+}
