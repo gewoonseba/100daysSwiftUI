7a186021aa5e26359b83b14a073b03042a2c6ed8
Sebastian Stoelen <gewoonseba@gmail.com>
2024-11-07 08:50:05 +0100

    day 62

---
+++ Projects/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Sebastian Stoelen on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
