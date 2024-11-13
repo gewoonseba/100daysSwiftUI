0f8fa65317dd5bab45aa8d84a0446f64767520f6
Sebastian Stoelen <gewoonseba@gmail.com>
2024-11-13 17:42:11 +0100

    day 68 part 2

--- Projects/Instafilter/BucketList/BucketList/ContentView.swift
+++ Projects/Instafilter/BucketList/BucketList/ContentView.swift
@@ -7,12 +7,39 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
     var body: some View {
-        Button("Read and Write") {
-            FileManager.write(content: "Test Message", filename: "test.txt")
-            print(FileManager.read(filename: "test.txt"))
-        }
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct ContentView: View {
+    @State private var loadingState = LoadingState.loading
+    
+    var body: some View {
+        if loadingState == .loading {
+            LoadingView()
+        } else if loadingState == .success {
+            SuccessView()
+        } else if loadingState == .failed {
+            FailedView()
+        }   
     }
 }
 
