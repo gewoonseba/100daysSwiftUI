0099faa58b7198e0a934c6ffa418f6af0db46c8a
Sebastian Stoelen <gewoonseba@gmail.com>
2024-11-15 08:37:52 +0100

    day 69: MapKit

--- Projects/Instafilter/BucketList/BucketList/ContentView.swift
+++ Projects/Instafilter/BucketList/BucketList/ContentView.swift
@@ -5,65 +5,62 @@
 //  Created by Sebastian Stoelen on 13/11/2024.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    @State private var loadingState = LoadingState.loading
-    
-    var body: some View {
-        if loadingState == .loading {
-            LoadingView()
-        } else if loadingState == .success {
-            SuccessView()
-        } else if loadingState == .failed {
-            FailedView()
-        }   
-    }
-}
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
 
-extension FileManager {
-    static func read(filename: String) -> String {
-        let url = URL.documentsDirectory.appending(path: filename)
-    
-        do {
-            return try String(contentsOf: url, encoding: .utf8)
-        } catch {
-            print(error.localizedDescription)
-            return ""
-        }
-    }
-    
-    static func write(content: String, filename: String) {
-        let url = URL.documentsDirectory.appending(path: filename)
-        
-        let data = Data(content.utf8)
-        
-        do {
-            try data.write(to: url, options: [.completeFileProtection, .atomic])
-        } catch {
-            print(error.localizedDescription)
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+
+    var body: some View {
+        VStack {
+            MapReader { proxy in
+                Map {
+                    ForEach(locations) { location in
+                        Marker(location.name, coordinate: location.coordinate)
+                    }
+                }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        print(coordinate)
+                    }
+                }
+            }
+
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
         }
     }
 }
