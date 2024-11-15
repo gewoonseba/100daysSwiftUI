a1603a65f7a18caa8bc135262ed23f80b8e1d08c
Sebastian Stoelen <gewoonseba@gmail.com>
2024-11-15 08:48:22 +0100

    day 69: face id

--- Projects/Instafilter/BucketList/BucketList/ContentView.swift
+++ Projects/Instafilter/BucketList/BucketList/ContentView.swift
@@ -5,62 +5,48 @@
 //  Created by Sebastian Stoelen on 13/11/2024.
 //
 
-import MapKit
-import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
+import SwiftUI
+import LocalAuthentication
+
+
 
 struct ContentView: View {
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        )
-    )
-
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
-
+    
+    @State private var isUnlocked = false
+    
+    
     var body: some View {
         VStack {
-            MapReader { proxy in
-                Map {
-                    ForEach(locations) { location in
-                        Marker(location.name, coordinate: location.coordinate)
-                    }
-                }
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        print(coordinate)
-                    }
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+                } else {
+                    // there was a problem
                 }
             }
-
-            HStack(spacing: 50) {
-                Button("Paris") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-
-                Button("Tokyo") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-            }
+        } else {
+            // no biometrics
         }
     }
 }
