f209674d792b1da9677c721c80b3efc29f84ef5e
Sebastian Stoelen <gewoonseba@gmail.com>
2024-11-17 14:38:08 +0100

    day 70

--- Projects/Instafilter/BucketList/BucketList/ContentView.swift
+++ Projects/Instafilter/BucketList/BucketList/ContentView.swift
@@ -9,12 +9,14 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
 
     let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.16, longitude: 4.45),
-            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
         )
     )
 
@@ -22,12 +24,30 @@ struct ContentView: View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
                 ForEach(locations) { location in
-                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onTapGesture {
+                                print("tap")
+                                selectedPlace = location
+                            }
+                    }
                 }
             }
             .onTapGesture { position in
                 if let coordinate = proxy.convert(position, from: .local) {
                     addToLocations(coordinate: coordinate)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
                 }
             }
         }
