01c7d5a135682f6d02c7e64142142967075b8617
Sebastian Stoelen <gewoonseba@gmail.com>
2024-11-28 13:51:40 +0100

    day 79

---
+++ Projects/HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Sebastian Stoelen on 28/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedTab = "One"
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Sebastian Stoelen on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
