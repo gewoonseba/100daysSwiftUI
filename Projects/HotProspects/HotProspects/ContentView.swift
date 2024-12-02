a957b4bc903880d8015dede3128cd5cf26c007f3
Sebastian Stoelen <gewoonseba@gmail.com>
2024-12-02 18:14:26 +0100

    day 81

--- Projects/HotProspects/HotProspects/ContentView.swift
+++ Projects/HotProspects/HotProspects/ContentView.swift
@@ -5,33 +5,21 @@
 //  Created by Sebastian Stoelen on 28/11/2024.
 //
 
+import SamplePackage
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
+    let possibleNumbers = 1 ... 60
+
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.formatted()
+    }
 
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .padding()
-                .background(backgroundColor)
-
-            Text("Change Color")
-                .padding()
-                .contextMenu {
-                    Button("Red", systemImage: "checkmark.circle.fill") {
-                        backgroundColor = .red
-                    }
-
-                    Button("Green") {
-                        backgroundColor = .green
-                    }
-
-                    Button("Blue") {
-                        backgroundColor = .blue
-                    }
-                }
-        }
+        Text(results)
     }
 }
 
