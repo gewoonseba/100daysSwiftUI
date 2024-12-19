b51262d93e973230b1b7f7b12295dda0aa08ba1d
Sebastian Stoelen <gewoonseba@gmail.com>
2024-12-19 08:50:29 +0100

    day 92: LayoutAndGeometry - absolute vs relative positioning

--- Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,36 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image(.profileDark)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                Text("Sebastian Stoelen")
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("Sebastian Stoelen")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-                Text("TEST")
-            }
-        }
+        Text("Hello, world!")
+            .offset(x: 100, y: 100)
+            .background(.red)
     }
-}
-
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
 }
 
 #Preview {
