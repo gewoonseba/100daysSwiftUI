f661eb3f912edea27fa93fbf59f6f9b5bc3e4e36
Sebastian Stoelen <gewoonseba@gmail.com>
2024-12-19 09:17:55 +0100

    Day 93: GeometryAndAlignment - Geometry Reader and coordinate spaces

--- Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,13 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct OuterView: View {
     var body: some View {
-        Text("Hello, world!")
-            .offset(x: 100, y: 100)
-            .background(.red)
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
     }
 }
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
+    }
+}
+
 
 #Preview {
     ContentView()
