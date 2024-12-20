c6b57fe77092221db43b0ec083acd1b42064a772
Sebastian Stoelen <gewoonseba@gmail.com>
2024-12-20 15:42:36 +0100

    Day 93: LayoutAndGeometry - Trying to improve album cover view

--- Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,24 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
-                    Text("Number \(num)")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(.red)
-                        .frame(width: 200, height: 200)
-                        .visualEffect { content, proxy in
-                            content
-                                .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+        GeometryReader { outer in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0) {
+                    ForEach(1..<20) { num in
+                        GeometryReader { inner in
+                            Text("Number \(num)")
+                                .font(.largeTitle)
+                                .padding()
+                                .background(.red)
+                            //try to get card in the middle perfectly non rotated
+                                .rotation3DEffect(.degrees(-(inner.frame(in: .global).center.x) - (outer.frame(in: .global).width / 2) / 8, axis: (x: 0, y: 1, z: 0))
+                                .frame(width: 200, height: 200)
                         }
-
+                        .frame(width: 200, height: 200)
+                    }
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
+        
     }
 }
 
