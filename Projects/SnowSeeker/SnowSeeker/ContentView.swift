4c3d5fbf5f17953323c1b0800c38d83216f339ab
Sebastian Stoelen <gewoonseba@gmail.com>
2024-12-23 08:30:30 +0100

    Day 96: SnowSeeker - Techniques

---
+++ Projects/SnowSeeker/SnowSeeker/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Sebastian Stoelen on 23/12/2024.
+//
+
+import SwiftUI
+
+@Observable
+class Player {
+    var name = "Anonymous"
+    var highScore = 0
+}
+
+struct HighScoreView: View {
+    @Environment(Player.self) var player
+
+    var body: some View {
+        @Bindable var player = player
+        Stepper("High score: \(player.highScore)", value: $player.highScore)
+    }
+}
+
+
+struct ContentView: View {
+    @State private var player = Player()
+
+    var body: some View {
+        VStack {
+            Text("Welcome!")
+            HighScoreView()
+        }
+        .environment(player)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Sebastian Stoelen on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
